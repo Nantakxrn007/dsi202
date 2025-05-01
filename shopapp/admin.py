0c98abd04dc50d7ece2597bf47650c678b76f328
Dinonon <eieizdy@gmail.com>
2025-05-01 22:48:04 +0700

    decorate product detail and connect with backend

--- shopapp/admin.py
+++ shopapp/admin.py
@@ -1,6 +1,14 @@
+# admin.py
 from django.contrib import admin
-from .models import Product
+from .models import Product, ProductOption
+
+class ProductOptionInline(admin.TabularInline):
+    model = ProductOption
+    extra = 1
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price')  
+    list_display = ['name', 'price', 'carbon_reduction']
+    inlines = [ProductOptionInline]
+
+admin.site.register(ProductOption)
--- shopapp/models.py
+++ shopapp/models.py
@@ -5,6 +5,17 @@ class Product(models.Model):
     image = models.ImageField(upload_to='products/')
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)  # 👈 เพิ่มตรงนี้
+    carbon_reduction = models.DecimalField(
+        max_digits=6, decimal_places=2, default=0.00,
+        help_text="จำนวนคาร์บอนที่ลดได้ (กิโลกรัม)"
+    )
 
     def __str__(self):
         return self.name
+    
+class ProductOption(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='options')
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.product.name} - {self.name}"
