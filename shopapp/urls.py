e8e354f02a97d3998fe129c85b07082c83ce4db2
Dinonon <eieizdy@gmail.com>
2025-05-13 17:08:21 +0700

    fix urls.py

--- shopapp/urls.py
+++ shopapp/urls.py
@@ -5,8 +5,8 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-        path('', FirstPageView.as_view(), name='first'),  # หน้าหลัก เริ่มที่ first
-        path('home/', HomePageView.as_view(), name='home'),
+        # path('', FirstPageView.as_view(), name='first'),  # หน้าหลัก เริ่มที่ first
+        path('', HomePageView.as_view(), name='home'),
         path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
         path('profile/', views.user_profile, name='user_profile'),
         path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
